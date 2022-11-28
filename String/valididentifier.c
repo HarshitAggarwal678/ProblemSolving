2403ccaecc564ef6c1c21fedd1bef5b13f1489ff
Harsh <aggarwalh678@gmail.com>
2022-11-28 22:55:55 +0530

    valid identifier

--- String/valididentifier.c
+++ String/valididentifier.c
@@ -7,12 +7,13 @@ void main()
     int flag=0;
     int i;
     if(a[0]=='_'||(a[0]>='a'&&a[0]<='z')||(a[0]>='A'&&a[0]<='Z'))
-    { i=1;}
-     else
-       {
+    { 
+        i=1;
+    }
+    else
+    {
         flag++;
-        
-       }
+    }
     while(a[i]!='\0')
     {
        if(!(a[i]=='_'||(a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z')||(a[i]>='0'&&a[i]<=9)||a[i]=='$'))
@@ -26,11 +27,9 @@ void main()
         
        }
     }
-
-    
-   
     if(flag==0)
-    {if(i<32)
+    {
+        if(i<32)
         printf("Yes");
 
     }
